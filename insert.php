eb7883383bb48f51f99ec743a67917d9395b2603
InkyChew <B10623019@yuntech.edu.tw>
2019-12-29 23:04:24 +0800

    css

--- insert.php
+++ insert.php
@@ -57,21 +57,21 @@
                                 $result = $conn->query($sql);
                                 $rows = $result->fetch_row();
                                 $rows = $rows[0] + 1;
-                                echo "memberID:" . $rows . "<br><br>";
+                                echo "memberID: " . $rows . "<br><br>";
                                 break;
                             case "外送員":
                                 $sql = "select MAX(deliveryStaffID) from deliverystaff";
                                 $result = $conn->query($sql);
                                 $rows = $result->fetch_row();
                                 $rows = $rows[0] + 1;
-                                echo "deliveryStaffID:" . $rows . "<br><br>";
+                                echo "deliveryStaffID: " . $rows . "<br><br>";
                                 break;
                             case "餐廳":
                                 $sql = "select MAX(restaurantID) from restaurant";
                                 $result = $conn->query($sql);
                                 $rows = $result->fetch_row();
                                 $rows = $rows[0] + 1;
-                                echo "restaurantID:" . $rows . "<br><br>";
+                                echo "restaurantID: " . $rows . "<br><br>";
                                 break;
                         } 
                     }else if($i ==5){
@@ -90,7 +90,7 @@
                         $result = $conn->query($sql);
                         $rows = $result->fetch_row();
                         $rows = $rows[0] + 1;
-                        echo "foodID:" . $rows . "<br><br>";
+                        echo "foodID: " . $rows . "<br><br>";
                     }else if($i == 1){
                         $field = $ary[$i];
                         $sql = "select restaurantID from restaurant";
@@ -124,7 +124,7 @@
                         $result = $conn->query($sql);
                         $rows = $result->fetch_row();
                         $rows = $rows[0] + 1;
-                        echo "orderId:" . $rows . "<br><br>";
+                        echo "orderId: " . $rows . "<br><br>";
                     }else if($i == 1){
                         $sql = "select memberID from member order by memberID ASC";
                     }else if($i == 2){
